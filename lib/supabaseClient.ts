2f4e1e5a8c633c531d7b978361bf71b0fa36cc4c
Idan-gabay <idan@citizencafetlv.com>
2025-07-19 15:18:23 +0300

    commit

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log(
+  "SUPABASE_SERVICE_ROLE_KEY:",
+  process.env.SUPABASE_SERVICE_ROLE_KEY ? "set" : "NOT SET"
+);
+
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
